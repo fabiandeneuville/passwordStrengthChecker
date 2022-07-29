7893ff95005e6362bb38e3e79cb87c65fad56cc8
Deneuville Fabian <deneuville.fabian@live.fr>
2022-07-29 13:14:49 +0200

    Password strength checker done

--- js/index.js
+++ js/index.js
@@ -1,20 +1,30 @@
 "use strict";
 /********** DOM ELEMENTS **********/
 const passwordInput = document.querySelector('.password');
+const face = document.querySelector('.animation');
 const pupils = document.querySelectorAll('.pupil');
 const mouth = document.querySelector('.mouth');
 const leftEyebrow = document.querySelector('.left-eyebrow');
 const rightEyebrow = document.querySelector('.right-eyebrow');
+const uppercasesText = document.querySelector('.uppercasesCheckText');
+const lowercasesText = document.querySelector('.lowercasesCheckText');
+const numbersCasesText = document.querySelector('.numbersCheckText');
+const symbolsCasesText = document.querySelector('.symbolsCheckText');
+const lengthCasesText = document.querySelector('.lengthCheckText');
 /********** VARIABLES **********/
 let posX, posY;
+/********** REGEXP **********/
 const passwordRegexp = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])(?!.* ).{8,}$/;
+const upperCasesRegexp = /[A-Z]/;
+const lowerCasesRegexp = /[a-z]/;
+const numbersRegexp = /[0-9]/;
+const symbolsRegexp = /[#?!@$%^&*-]/;
 /********** EVENT LISTENERS **********/
 passwordInput.addEventListener('keyup', (e) => {
     moveEyes(e);
     checkPassword(passwordInput.value, passwordRegexp);
 });
 /********** FUNCTIONS **********/
-// Moving eyes on input event
 function moveEyes(e) {
     let target = e.target;
     const cursorXpositions = target.selectionStart;
@@ -28,16 +38,42 @@ function moveEyes(e) {
         }
     });
 }
-// Animating face when password is checked
 function checkPassword(password, regexp) {
     if (password.match(regexp)) {
         mouth.style.transform = "translateX(-50%) rotate(360deg)";
         leftEyebrow.style.transform = "rotate(45deg)";
         rightEyebrow.style.transform = "rotate(-45deg)";
+        face.style.backgroundColor = "indianred";
     }
     else {
         mouth.style.transform = "translateX(-50%) rotate(180deg)";
         leftEyebrow.style.transform = "rotate(0)";
         rightEyebrow.style.transform = "rotate(0)";
+        face.style.backgroundColor = "antiquewhite";
+    }
+    displayText(passwordInput, uppercasesText, upperCasesRegexp);
+    displayText(passwordInput, lowercasesText, lowerCasesRegexp);
+    displayText(passwordInput, numbersCasesText, numbersRegexp);
+    displayText(passwordInput, symbolsCasesText, symbolsRegexp);
+    checkLength(passwordInput, 8, lengthCasesText);
+}
+function displayText(input, text, regexp) {
+    if (input.value.match(regexp)) {
+        text.style.opacity = "1";
+        text.style.color = "green";
+    }
+    else {
+        text.style.opacity = "0.1";
+        text.style.color = "black";
+    }
+}
+function checkLength(input, minLength, text) {
+    if (input.value.length >= minLength) {
+        text.style.opacity = "1";
+        text.style.color = "green";
+    }
+    else {
+        text.style.opacity = "0.1";
+        text.style.color = "black";
     }
 }
--- ts/index.ts
+++ ts/index.ts
@@ -1,16 +1,30 @@
 /********** DOM ELEMENTS **********/
 
 const passwordInput = document.querySelector('.password') as HTMLInputElement;
+const face = document.querySelector('.animation') as HTMLDivElement;
 const pupils = document.querySelectorAll('.pupil') as NodeListOf<HTMLDivElement>;
 const mouth = document.querySelector('.mouth') as HTMLDivElement;
 const leftEyebrow = document.querySelector('.left-eyebrow') as HTMLDivElement;
 const rightEyebrow = document.querySelector('.right-eyebrow') as HTMLDivElement;
 
+const uppercasesText = document.querySelector('.uppercasesCheckText') as HTMLParagraphElement;
+const lowercasesText = document.querySelector('.lowercasesCheckText') as HTMLParagraphElement;
+const numbersCasesText = document.querySelector('.numbersCheckText') as HTMLParagraphElement;
+const symbolsCasesText = document.querySelector('.symbolsCheckText') as HTMLParagraphElement;
+const lengthCasesText = document.querySelector('.lengthCheckText') as HTMLParagraphElement;
+
 /********** VARIABLES **********/
 
 let posX : string, posY : string
 
+/********** REGEXP **********/
+
 const passwordRegexp : RegExp = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])(?!.* ).{8,}$/
+
+const upperCasesRegexp : RegExp = /[A-Z]/;
+const lowerCasesRegexp : RegExp = /[a-z]/;
+const numbersRegexp : RegExp = /[0-9]/;
+const symbolsRegexp : RegExp = /[#?!@$%^&*-]/;
 
 /********** EVENT LISTENERS **********/
 
@@ -22,8 +36,6 @@ passwordInput.addEventListener('keyup', (e : KeyboardEvent) => {
 })
 
 /********** FUNCTIONS **********/
-
-// Moving eyes on input event
 
 function moveEyes(e : KeyboardEvent) : void {
 
@@ -40,20 +52,49 @@ function moveEyes(e : KeyboardEvent) : void {
 
 }
 
-// Animating face when password is checked
-
 function checkPassword(password: string, regexp: RegExp) : void {
 
     if(password.match(regexp)){
         mouth.style.transform = "translateX(-50%) rotate(360deg)"
         leftEyebrow.style.transform = "rotate(45deg)";
-        rightEyebrow.style.transform = "rotate(-45deg)"
+        rightEyebrow.style.transform = "rotate(-45deg)";
+        face.style.backgroundColor = "indianred";
     } else {
         mouth.style.transform = "translateX(-50%) rotate(180deg)"
         leftEyebrow.style.transform = "rotate(0)";
-        rightEyebrow.style.transform = "rotate(0)"
+        rightEyebrow.style.transform = "rotate(0)";
+        face.style.backgroundColor = "antiquewhite";
     }
+
+    displayText(passwordInput, uppercasesText, upperCasesRegexp);
+    displayText(passwordInput, lowercasesText, lowerCasesRegexp);
+    displayText(passwordInput, numbersCasesText, numbersRegexp);
+    displayText(passwordInput, symbolsCasesText, symbolsRegexp);
+    checkLength(passwordInput, 8, lengthCasesText);
+    
 }
 
+function displayText(input : HTMLInputElement, text: HTMLParagraphElement, regexp : RegExp){
 
+    if(input.value.match(regexp)){
+        text.style.opacity = "1";
+        text.style.color = "green";
+    } else {
+        text.style.opacity = "0.1";
+        text.style.color = "black";
+    }
+
+}
+
+function checkLength(input: HTMLInputElement, minLength: number, text : HTMLParagraphElement){
+
+    if(input.value.length >= minLength){
+        text.style.opacity = "1";
+        text.style.color = "green";
+    } else {
+        text.style.opacity = "0.1";
+        text.style.color = "black";
+    }
+
+}
 
