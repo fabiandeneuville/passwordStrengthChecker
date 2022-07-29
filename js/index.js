c05610e9c23344cb540c652f0f27f72e4836e6cf
Deneuville Fabian <deneuville.fabian@live.fr>
2022-07-29 16:54:08 +0200

    Adding info on time needed to crack the password

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 "use strict";
 /********** DOM ELEMENTS **********/
+const body = document.querySelector('body');
 const passwordInput = document.querySelector('.password');
 const face = document.querySelector('.animation');
 const pupils = document.querySelectorAll('.pupil');
@@ -11,14 +12,26 @@ const lowercasesText = document.querySelector('.lowercasesCheckText');
 const numbersCasesText = document.querySelector('.numbersCheckText');
 const symbolsCasesText = document.querySelector('.symbolsCheckText');
 const lengthCasesText = document.querySelector('.lengthCheckText');
+const result = document.querySelector('.result');
 /********** VARIABLES **********/
 let posX, posY;
+const verybad = 'crimson';
+const bad = 'orange';
+const average = 'yellow';
+const good = 'lightgreen';
+const verygood = 'green';
 /********** REGEXP **********/
-const passwordRegexp = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])(?!.* ).{8,}$/;
+const passwordRegexp = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])(?!.* ).{16,}$/;
 const upperCasesRegexp = /[A-Z]/;
 const lowerCasesRegexp = /[a-z]/;
 const numbersRegexp = /[0-9]/;
 const symbolsRegexp = /[#?!@$%^&*-]/;
+const onlyNumbers = /^[0-9]+$/;
+const onlyLowercases = /^[a-z]+$/;
+const onlyUppercases = /^[A-Z]+$/;
+const onlyLetters = /^[a-zA-Z]+$/;
+const lettersAndNumbers = /^[a-zA-Z0-9]+$/;
+const lettersNumbersAndSymbols = /^[a-zA-Z0-9#?!@$%^&*-_]+$/;
 /********** EVENT LISTENERS **********/
 passwordInput.addEventListener('keyup', (e) => {
     moveEyes(e);
@@ -55,7 +68,8 @@ function checkPassword(password, regexp) {
     displayText(passwordInput, lowercasesText, lowerCasesRegexp);
     displayText(passwordInput, numbersCasesText, numbersRegexp);
     displayText(passwordInput, symbolsCasesText, symbolsRegexp);
-    checkLength(passwordInput, 8, lengthCasesText);
+    checkLength(passwordInput, 16, lengthCasesText);
+    estimateCrackingTime(password);
 }
 function displayText(input, text, regexp) {
     if (input.value.match(regexp)) {
@@ -75,5 +89,292 @@ function checkLength(input, minLength, text) {
     else {
         text.style.opacity = "0.1";
         text.style.color = "black";
+    }
+}
+function estimateCrackingTime(password) {
+    // ONLY NUMBERS
+    if (password.length <= 11 && password.match(onlyNumbers)) {
+        result.textContent = "Instantly";
+        body.style.background = verybad;
+    }
+    else if (password.length === 12 && password.match(onlyNumbers)) {
+        result.textContent = "2 secs";
+        body.style.background = bad;
+    }
+    else if (password.length === 13 && password.match(onlyNumbers)) {
+        result.textContent = "19 secs";
+        body.style.background = bad;
+    }
+    else if (password.length === 14 && password.match(onlyNumbers)) {
+        result.textContent = "3 mins";
+        body.style.background = bad;
+    }
+    else if (password.length === 15 && password.match(onlyNumbers)) {
+        result.textContent = "32 mins";
+        body.style.background = bad;
+    }
+    else if (password.length === 16 && password.match(onlyNumbers)) {
+        result.textContent = "5 hours";
+        body.style.background = bad;
+    }
+    else if (password.length === 17 && password.match(onlyNumbers)) {
+        result.textContent = "2 days";
+        body.style.background = bad;
+    }
+    else if (password.length >= 18 && password.match(onlyNumbers)) {
+        result.textContent = "3 weeks";
+        body.style.background = bad;
+    }
+    // ONLY LOWERCASES
+    else if (password.length <= 8 && password.match(onlyLowercases)) {
+        result.textContent = "Instantly";
+        body.style.background = verybad;
+    }
+    else if (password.length === 9 && password.match(onlyLowercases)) {
+        result.textContent = "10 secs";
+        body.style.background = bad;
+    }
+    else if (password.length === 10 && password.match(onlyLowercases)) {
+        result.textContent = "4 mins";
+        body.style.background = bad;
+    }
+    else if (password.length === 11 && password.match(onlyLowercases)) {
+        result.textContent = "2 hours";
+        body.style.background = bad;
+    }
+    else if (password.length === 12 && password.match(onlyLowercases)) {
+        result.textContent = "2 days";
+        body.style.background = bad;
+    }
+    else if (password.length === 13 && password.match(onlyLowercases)) {
+        result.textContent = "2 months";
+        body.style.background = bad;
+    }
+    else if (password.length === 14 && password.match(onlyLowercases)) {
+        result.textContent = "4 years";
+        body.style.background = average;
+    }
+    else if (password.length === 15 && password.match(onlyLowercases)) {
+        result.textContent = "100 years";
+        body.style.background = average;
+    }
+    else if (password.length === 16 && password.match(onlyLowercases)) {
+        result.textContent = "3k years";
+        body.style.background = average;
+    }
+    else if (password.length === 17 && password.match(onlyLowercases)) {
+        result.textContent = "69k years";
+        body.style.background = average;
+    }
+    else if (password.length >= 18 && password.match(onlyLowercases)) {
+        result.textContent = "2m years";
+        body.style.background = good;
+    }
+    // ONLY UPERCASES
+    else if (password.length <= 8 && password.match(onlyUppercases)) {
+        result.textContent = "Instantly";
+        body.style.background = verybad;
+    }
+    else if (password.length === 9 && password.match(onlyUppercases)) {
+        result.textContent = "10 secs";
+        body.style.background = bad;
+    }
+    else if (password.length === 10 && password.match(onlyUppercases)) {
+        result.textContent = "4 mins";
+        body.style.background = bad;
+    }
+    else if (password.length === 11 && password.match(onlyUppercases)) {
+        result.textContent = "2 hours";
+        body.style.background = bad;
+    }
+    else if (password.length === 12 && password.match(onlyUppercases)) {
+        result.textContent = "2 days";
+        body.style.background = bad;
+    }
+    else if (password.length === 13 && password.match(onlyUppercases)) {
+        result.textContent = "2 months";
+        body.style.background = bad;
+    }
+    else if (password.length === 14 && password.match(onlyUppercases)) {
+        result.textContent = "4 years";
+        body.style.background = average;
+    }
+    else if (password.length === 15 && password.match(onlyUppercases)) {
+        result.textContent = "100 years";
+        body.style.background = average;
+    }
+    else if (password.length === 16 && password.match(onlyUppercases)) {
+        result.textContent = "3k years";
+        body.style.background = average;
+    }
+    else if (password.length === 17 && password.match(onlyUppercases)) {
+        result.textContent = "69k years";
+        body.style.background = average;
+    }
+    else if (password.length >= 18 && password.match(onlyUppercases)) {
+        result.textContent = "2m years";
+        body.style.background = good;
+    }
+    // ONLY LETTERS (LOWERCASES AND UPPERCASES)
+    else if (password.length <= 6 && password.match(onlyLetters)) {
+        result.textContent = "Instantly";
+        body.style.background = verybad;
+    }
+    else if (password.length === 7 && password.match(onlyLetters)) {
+        result.textContent = "2 secs";
+        body.style.background = bad;
+    }
+    else if (password.length === 8 && password.match(onlyLetters)) {
+        result.textContent = "2 mins";
+        body.style.background = bad;
+    }
+    else if (password.length === 9 && password.match(onlyLetters)) {
+        result.textContent = "1 hour";
+        body.style.background = bad;
+    }
+    else if (password.length === 10 && password.match(onlyLetters)) {
+        result.textContent = "3 days";
+        body.style.background = bad;
+    }
+    else if (password.length === 11 && password.match(onlyLetters)) {
+        result.textContent = "5 months";
+        body.style.background = bad;
+    }
+    else if (password.length === 12 && password.match(onlyLetters)) {
+        result.textContent = "24 years";
+        body.style.background = average;
+    }
+    else if (password.length === 13 && password.match(onlyLetters)) {
+        result.textContent = "1k years";
+        body.style.background = average;
+    }
+    else if (password.length === 14 && password.match(onlyLetters)) {
+        result.textContent = "64k years";
+        body.style.background = average;
+    }
+    else if (password.length === 15 && password.match(onlyLetters)) {
+        result.textContent = "3m years";
+        body.style.background = good;
+    }
+    else if (password.length === 16 && password.match(onlyLetters)) {
+        result.textContent = "173m years";
+        body.style.background = good;
+    }
+    else if (password.length === 17 && password.match(onlyLetters)) {
+        result.textContent = "9bn years";
+        body.style.background = good;
+    }
+    else if (password.length >= 18 && password.match(onlyLetters)) {
+        result.textContent = "467bn years";
+        body.style.background = verygood;
+    }
+    // LETTERS (LOWERCASES AND UPPERCASES) AND NUMBERS
+    else if (password.length <= 6 && password.match(lettersAndNumbers)) {
+        result.textContent = "Instantly";
+        body.style.background = verybad;
+    }
+    else if (password.length === 7 && password.match(lettersAndNumbers)) {
+        result.textContent = "7 secs";
+        body.style.background = bad;
+    }
+    else if (password.length === 8 && password.match(lettersAndNumbers)) {
+        result.textContent = "7 mins";
+        body.style.background = bad;
+    }
+    else if (password.length === 9 && password.match(lettersAndNumbers)) {
+        result.textContent = "7 hours";
+        body.style.background = bad;
+    }
+    else if (password.length === 10 && password.match(lettersAndNumbers)) {
+        result.textContent = "3 weeks";
+        body.style.background = bad;
+    }
+    else if (password.length === 11 && password.match(lettersAndNumbers)) {
+        result.textContent = "3 years";
+        body.style.background = average;
+    }
+    else if (password.length === 12 && password.match(lettersAndNumbers)) {
+        result.textContent = "200 years";
+        body.style.background = average;
+    }
+    else if (password.length === 13 && password.match(lettersAndNumbers)) {
+        result.textContent = "12k years";
+        body.style.background = average;
+    }
+    else if (password.length === 14 && password.match(lettersAndNumbers)) {
+        result.textContent = "750k years";
+        body.style.background = good;
+    }
+    else if (password.length === 15 && password.match(lettersAndNumbers)) {
+        result.textContent = "46m years";
+        body.style.background = good;
+    }
+    else if (password.length === 16 && password.match(lettersAndNumbers)) {
+        result.textContent = "3bn years";
+        body.style.background = good;
+    }
+    else if (password.length === 17 && password.match(lettersAndNumbers)) {
+        result.textContent = "179bn years";
+        body.style.background = verygood;
+    }
+    else if (password.length >= 18 && password.match(lettersAndNumbers)) {
+        result.textContent = "11tn years";
+        body.style.background = verygood;
+    }
+    // LETTERS (LOWERCASES AND UPPERCASES), NUMBERS AND SYMBOLS
+    else if (password.length <= 6 && password.match(lettersNumbersAndSymbols)) {
+        result.textContent = "Instantly";
+        body.style.background = verybad;
+    }
+    else if (password.length === 7 && password.match(lettersNumbersAndSymbols)) {
+        result.textContent = "31 secs";
+        body.style.background = bad;
+    }
+    else if (password.length === 8 && password.match(lettersNumbersAndSymbols)) {
+        result.textContent = "39 mins";
+        body.style.background = bad;
+    }
+    else if (password.length === 9 && password.match(lettersNumbersAndSymbols)) {
+        result.textContent = "2 days";
+        body.style.background = bad;
+    }
+    else if (password.length === 10 && password.match(lettersNumbersAndSymbols)) {
+        result.textContent = "5 months";
+        body.style.background = bad;
+    }
+    else if (password.length === 11 && password.match(lettersNumbersAndSymbols)) {
+        result.textContent = "34 years";
+        body.style.background = average;
+    }
+    else if (password.length === 12 && password.match(lettersNumbersAndSymbols)) {
+        result.textContent = "3k years";
+        body.style.background = average;
+    }
+    else if (password.length === 13 && password.match(lettersNumbersAndSymbols)) {
+        result.textContent = "202k years";
+        body.style.background = good;
+    }
+    else if (password.length === 14 && password.match(lettersNumbersAndSymbols)) {
+        result.textContent = "16m years";
+        body.style.background = good;
+    }
+    else if (password.length === 15 && password.match(lettersNumbersAndSymbols)) {
+        result.textContent = "1bn years";
+        body.style.background = good;
+    }
+    else if (password.length === 16 && password.match(lettersNumbersAndSymbols)) {
+        result.textContent = "92bn years";
+        body.style.background = verygood;
+    }
+    else if (password.length === 17 && password.match(lettersNumbersAndSymbols)) {
+        result.textContent = "7tn years";
+        body.style.background = verygood;
+    }
+    else if (password.length >= 18 && password.match(lettersNumbersAndSymbols)) {
+        result.textContent = "438tn years";
+        body.style.background = verygood;
+    }
+    else {
+        result.textContent = "??";
     }
 }
--- ts/index.ts
+++ ts/index.ts
@@ -1,5 +1,6 @@
 /********** DOM ELEMENTS **********/
 
+const body = document.querySelector('body') as HTMLBodyElement;
 const passwordInput = document.querySelector('.password') as HTMLInputElement;
 const face = document.querySelector('.animation') as HTMLDivElement;
 const pupils = document.querySelectorAll('.pupil') as NodeListOf<HTMLDivElement>;
@@ -13,18 +14,33 @@ const numbersCasesText = document.querySelector('.numbersCheckText') as HTMLPara
 const symbolsCasesText = document.querySelector('.symbolsCheckText') as HTMLParagraphElement;
 const lengthCasesText = document.querySelector('.lengthCheckText') as HTMLParagraphElement;
 
+const result = document.querySelector('.result') as HTMLSpanElement;
+
 /********** VARIABLES **********/
 
 let posX : string, posY : string
 
+const verybad : string = 'crimson';
+const bad : string = 'orange';
+const average : string = 'yellow';
+const good : string = 'lightgreen';
+const verygood : string = 'green';
+
 /********** REGEXP **********/
 
-const passwordRegexp : RegExp = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])(?!.* ).{8,}$/
+const passwordRegexp : RegExp = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])(?!.* ).{16,}$/
 
 const upperCasesRegexp : RegExp = /[A-Z]/;
 const lowerCasesRegexp : RegExp = /[a-z]/;
 const numbersRegexp : RegExp = /[0-9]/;
 const symbolsRegexp : RegExp = /[#?!@$%^&*-]/;
+
+const onlyNumbers : RegExp = /^[0-9]+$/;
+const onlyLowercases : RegExp = /^[a-z]+$/;
+const onlyUppercases : RegExp = /^[A-Z]+$/;
+const onlyLetters : RegExp = /^[a-zA-Z]+$/;
+const lettersAndNumbers : RegExp = /^[a-zA-Z0-9]+$/;
+const lettersNumbersAndSymbols : RegExp = /^[a-zA-Z0-9#?!@$%^&*-_]+$/;
 
 /********** EVENT LISTENERS **********/
 
@@ -70,7 +86,9 @@ function checkPassword(password: string, regexp: RegExp) : void {
     displayText(passwordInput, lowercasesText, lowerCasesRegexp);
     displayText(passwordInput, numbersCasesText, numbersRegexp);
     displayText(passwordInput, symbolsCasesText, symbolsRegexp);
-    checkLength(passwordInput, 8, lengthCasesText);
+    checkLength(passwordInput, 16, lengthCasesText);
+
+    estimateCrackingTime(password)
     
 }
 
@@ -96,5 +114,229 @@ function checkLength(input: HTMLInputElement, minLength: number, text : HTMLPara
         text.style.color = "black";
     }
 
+}
+
+function estimateCrackingTime(password: string){
+    // ONLY NUMBERS
+    if(password.length <= 11 && password.match(onlyNumbers)){
+        result.textContent = "Instantly";
+        body.style.background = verybad;
+    } else if (password.length === 12 && password.match(onlyNumbers)) {
+        result.textContent = "2 secs";
+        body.style.background = bad;
+    } else if (password.length === 13 && password.match(onlyNumbers)) {
+        result.textContent = "19 secs";
+        body.style.background = bad;
+    } else if (password.length === 14 && password.match(onlyNumbers)) {
+        result.textContent = "3 mins";
+        body.style.background = bad;
+    } else if (password.length === 15 && password.match(onlyNumbers)) {
+        result.textContent = "32 mins";
+        body.style.background = bad;
+    } else if (password.length === 16 && password.match(onlyNumbers)) {
+        result.textContent = "5 hours";
+        body.style.background = bad;
+    } else if (password.length === 17 && password.match(onlyNumbers)) {
+        result.textContent = "2 days";
+        body.style.background = bad;
+    } else if (password.length >= 18 && password.match(onlyNumbers)) {
+        result.textContent = "3 weeks";
+        body.style.background = bad;
+    } 
+    // ONLY LOWERCASES
+    else if (password.length <= 8 && password.match(onlyLowercases)){
+        result.textContent = "Instantly";
+        body.style.background = verybad;
+    } else if (password.length === 9 && password.match(onlyLowercases)){
+        result.textContent = "10 secs";
+        body.style.background = bad;
+    } else if (password.length === 10 && password.match(onlyLowercases)){
+        result.textContent = "4 mins";
+        body.style.background = bad;
+    } else if (password.length === 11 && password.match(onlyLowercases)){
+        result.textContent = "2 hours";
+        body.style.background = bad;
+    } else if (password.length === 12 && password.match(onlyLowercases)){
+        result.textContent = "2 days";
+        body.style.background = bad;
+    } else if (password.length === 13 && password.match(onlyLowercases)){
+        result.textContent = "2 months";
+        body.style.background = bad;
+    } else if (password.length === 14 && password.match(onlyLowercases)){
+        result.textContent = "4 years";
+        body.style.background = average;
+    } else if (password.length === 15 && password.match(onlyLowercases)){
+        result.textContent = "100 years";
+        body.style.background = average;
+    } else if (password.length === 16 && password.match(onlyLowercases)){
+        result.textContent = "3k years";
+        body.style.background = average;
+    } else if (password.length === 17 && password.match(onlyLowercases)){
+        result.textContent = "69k years";
+        body.style.background = average;
+    } else if (password.length >= 18 && password.match(onlyLowercases)){
+        result.textContent = "2m years";
+        body.style.background = good;
+    }
+    // ONLY UPERCASES
+    else if (password.length <= 8 && password.match(onlyUppercases)){
+        result.textContent = "Instantly";
+        body.style.background = verybad;
+    } else if (password.length === 9 && password.match(onlyUppercases)){
+        result.textContent = "10 secs";
+        body.style.background = bad;
+    } else if (password.length === 10 && password.match(onlyUppercases)){
+        result.textContent = "4 mins";
+        body.style.background = bad;
+    } else if (password.length === 11 && password.match(onlyUppercases)){
+        result.textContent = "2 hours";
+        body.style.background = bad;
+    } else if (password.length === 12 && password.match(onlyUppercases)){
+        result.textContent = "2 days";
+        body.style.background = bad;
+    } else if (password.length === 13 && password.match(onlyUppercases)){
+        result.textContent = "2 months";
+        body.style.background = bad;
+    } else if (password.length === 14 && password.match(onlyUppercases)){
+        result.textContent = "4 years";
+        body.style.background = average;
+    } else if (password.length === 15 && password.match(onlyUppercases)){
+        result.textContent = "100 years";
+        body.style.background = average;
+    } else if (password.length === 16 && password.match(onlyUppercases)){
+        result.textContent = "3k years";
+        body.style.background = average;
+    } else if (password.length === 17 && password.match(onlyUppercases)){
+        result.textContent = "69k years";
+        body.style.background = average;
+    } else if (password.length >= 18 && password.match(onlyUppercases)){
+        result.textContent = "2m years";
+        body.style.background = good;
+    }
+    // ONLY LETTERS (LOWERCASES AND UPPERCASES)
+    else if (password.length <= 6 && password.match(onlyLetters)){
+        result.textContent = "Instantly";
+        body.style.background = verybad;
+    } else if (password.length === 7 && password.match(onlyLetters)){
+        result.textContent = "2 secs";
+        body.style.background = bad;
+    } else if (password.length === 8 && password.match(onlyLetters)){
+        result.textContent = "2 mins";
+        body.style.background = bad;
+    } else if (password.length === 9 && password.match(onlyLetters)){
+        result.textContent = "1 hour";
+        body.style.background = bad;
+    } else if (password.length === 10 && password.match(onlyLetters)){
+        result.textContent = "3 days";
+        body.style.background = bad;
+    } else if (password.length === 11 && password.match(onlyLetters)){
+        result.textContent = "5 months";
+        body.style.background = bad;
+    } else if (password.length === 12 && password.match(onlyLetters)){
+        result.textContent = "24 years";
+        body.style.background = average;
+    } else if (password.length === 13 && password.match(onlyLetters)){
+        result.textContent = "1k years";
+        body.style.background = average;
+    } else if (password.length === 14 && password.match(onlyLetters)){
+        result.textContent = "64k years";
+        body.style.background = average;
+    } else if (password.length === 15 && password.match(onlyLetters)){
+        result.textContent = "3m years";
+        body.style.background = good;
+    } else if (password.length === 16 && password.match(onlyLetters)){
+        result.textContent = "173m years";
+        body.style.background = good;
+    } else if (password.length === 17 && password.match(onlyLetters)){
+        result.textContent = "9bn years";
+        body.style.background = good;
+    } else if (password.length >= 18 && password.match(onlyLetters)){
+        result.textContent = "467bn years";
+        body.style.background = verygood;
+    }
+    // LETTERS (LOWERCASES AND UPPERCASES) AND NUMBERS
+    else if (password.length <= 6 && password.match(lettersAndNumbers)){
+        result.textContent = "Instantly";
+        body.style.background = verybad;
+    } else if (password.length === 7 && password.match(lettersAndNumbers)){
+        result.textContent = "7 secs";
+        body.style.background = bad;
+    } else if (password.length === 8 && password.match(lettersAndNumbers)){
+        result.textContent = "7 mins";
+        body.style.background = bad;
+    } else if (password.length === 9 && password.match(lettersAndNumbers)){
+        result.textContent = "7 hours";
+        body.style.background = bad;
+    } else if (password.length === 10 && password.match(lettersAndNumbers)){
+        result.textContent = "3 weeks";
+        body.style.background = bad;
+    } else if (password.length === 11 && password.match(lettersAndNumbers)){
+        result.textContent = "3 years";
+        body.style.background = average;
+    } else if (password.length === 12 && password.match(lettersAndNumbers)){
+        result.textContent = "200 years";
+        body.style.background = average;
+    } else if (password.length === 13 && password.match(lettersAndNumbers)){
+        result.textContent = "12k years";
+        body.style.background = average;
+    } else if (password.length === 14 && password.match(lettersAndNumbers)){
+        result.textContent = "750k years";
+        body.style.background = good;
+    } else if (password.length === 15 && password.match(lettersAndNumbers)){
+        result.textContent = "46m years";
+        body.style.background = good;
+    } else if (password.length === 16 && password.match(lettersAndNumbers)){
+        result.textContent = "3bn years";
+        body.style.background = good;
+    } else if (password.length === 17 && password.match(lettersAndNumbers)){
+        result.textContent = "179bn years";
+        body.style.background = verygood;
+    } else if (password.length >= 18 && password.match(lettersAndNumbers)){
+        result.textContent = "11tn years";
+        body.style.background = verygood;
+    }
+    // LETTERS (LOWERCASES AND UPPERCASES), NUMBERS AND SYMBOLS
+    else if (password.length <= 6 && password.match(lettersNumbersAndSymbols)){
+        result.textContent = "Instantly";
+        body.style.background = verybad;
+    } else if (password.length === 7 && password.match(lettersNumbersAndSymbols)){
+        result.textContent = "31 secs";
+        body.style.background = bad;
+    } else if (password.length === 8 && password.match(lettersNumbersAndSymbols)){
+        result.textContent = "39 mins";
+        body.style.background = bad;
+    } else if (password.length === 9 && password.match(lettersNumbersAndSymbols)){
+        result.textContent = "2 days";
+        body.style.background = bad;
+    } else if (password.length === 10 && password.match(lettersNumbersAndSymbols)){
+        result.textContent = "5 months";
+        body.style.background = bad;
+    } else if (password.length === 11 && password.match(lettersNumbersAndSymbols)){
+        result.textContent = "34 years";
+        body.style.background = average;
+    } else if (password.length === 12 && password.match(lettersNumbersAndSymbols)){
+        result.textContent = "3k years";
+        body.style.background = average;
+    } else if (password.length === 13 && password.match(lettersNumbersAndSymbols)){
+        result.textContent = "202k years";
+        body.style.background = good;
+    } else if (password.length === 14 && password.match(lettersNumbersAndSymbols)){
+        result.textContent = "16m years";
+        body.style.background = good;
+    } else if (password.length === 15 && password.match(lettersNumbersAndSymbols)){
+        result.textContent = "1bn years";
+        body.style.background = good;
+    } else if (password.length === 16 && password.match(lettersNumbersAndSymbols)){
+        result.textContent = "92bn years";
+        body.style.background = verygood;
+    } else if (password.length === 17 && password.match(lettersNumbersAndSymbols)){
+        result.textContent = "7tn years";
+        body.style.background = verygood;
+    } else if (password.length >= 18 && password.match(lettersNumbersAndSymbols)){
+        result.textContent = "438tn years";
+        body.style.background = verygood;
+    } else {
+        result.textContent = "??"
+    }
 }
 
